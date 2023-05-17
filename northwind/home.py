d06730ce14384132d1fcdaef685933b5cfa9122e
Biraj Koirala <koiralabiraj@gmail.com>
2023-05-17 23:58:14 +0545

    show table/add region feature added

--- northwind/home.py
+++ northwind/home.py
@@ -1,6 +1,7 @@
 from flask import render_template,request,redirect,url_for
 from northwind import application
 from northwind.helper import connect_to_db
+import random
 
 @application.route('/', methods=['GET', 'POST'])
 def home():
@@ -11,7 +12,7 @@ def home():
 def result():
     if request.method == 'POST':
         action = request.form.get('action')
-        if action == 'show_table':
+        if action == 'show_order_table':
             # Perform action to show all table data
             conn = connect_to_db()
             cursor = conn.cursor()
@@ -19,17 +20,30 @@ def result():
             result = cursor.fetchall()
             return render_template('result.html', table_data=result)
 
-        elif action == 'add_row':
-            # Perform action to add a row
-            # add_row_function()
-            result = "New row added"
+        elif action == 'show_employee_table':
+            conn = connect_to_db()
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM employees")
+            result = cursor.fetchall()
             return render_template('result.html', table_data=result)
 
-        elif action == 'delete_value':
-            # Perform action to delete a value
-            # delete_value_function()
-            result = "Value deleted"
+        elif action == 'show_region_table':
+            conn = connect_to_db()
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM region")
+            result = cursor.fetchall()
             return render_template('result.html', table_data=result)
+        
+        elif action == 'add_region':
+            conn = connect_to_db()
+            cursor = conn.cursor()
+            region_value = request.form.get('add_region')
+            random_number = random.randint(10, 100000)
+            cursor.execute("INSERT INTO region (region_id,region_description) VALUES (%s,%s)", (random_number,region_value))
+            conn.commit()
+            result = 'Region Added Succesfully'
+            return render_template('result.html', table_data=result)
+        
 
     return redirect(url_for('home'))
 
